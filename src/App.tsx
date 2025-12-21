0146d6325ab026665d234cc4d1ddf068e69d8067
Samuel Garcia <sam.garcia@dal.ca>
2025-12-21 06:30:25 -0500

    Header and upload section

--- src/App.tsx
+++ src/App.tsx
@@ -1,32 +1,19 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
+      <div className="ms-5 mt-5">
+        <h1 className="text-2xl font-bold">Snapchat Friends</h1>
+        <p className="mt-2 text-gray-600">
+          Easily search through your Snapchat friends by username or display name. <br />
+          You can also filter based on date added and friendship status.
         </p>
       </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+
+      <div className="flex items-center gap-2 mt-10 mx-5">
+        <h2 className="text-lg font-bold">File Upload:</h2>
+        <button id="uploadButton" className="bg-cyan-500 text-white px-4 py-2 rounded">Upload</button>
+        <input id="contentFile" type="file" accept=".json"/>
+      </div>
     </>
   )
 }
